e18eccdc0df0467c8fd1dec16f600db7ec49bbda
Fuzz Leonard <fuzz@fuzzleonard.com>
2014-01-28 22:54:39 -0800

    remove commafy serializer

--- lib/moe.rb
+++ lib/moe.rb
@@ -5,7 +5,6 @@ require "securerandom"
 require "moe/config"
 require "moe/dyna"
 require "moe/models/sequence"
-require "moe/serializers/commafy"
 require "moe/table_manager"
 require "moe/version"
 
