ad29ad37243c5478cb6b815cf6278efe657f8f77
Fuzz Leonard <fuzz@fuzzleonard.com>
2014-01-30 22:44:37 -0800

    Cleanup

--- lib/moe/sequence/collection.rb
+++ lib/moe/sequence/collection.rb
@@ -1,49 +1,47 @@
 module Moe
   module Sequence
     class Collection
-      attr_accessor :dyna, :sequences
-      attr_reader   :owner_id, :read_tables, :uuid
+      attr_accessor :dyna
+      attr_reader   :owner_id, :read_tables
 
       def initialize(name, owner_id)
-        @dyna          = Dyna.new
-        @sequences     = []
-        @owner_id      = owner_id
-        @read_tables   = Moe.config.tables[name].first
+        @dyna         = Dyna.new
+        @owner_id     = owner_id
+        @read_tables  = Moe.config.tables[name].first
       end
 
       def get_metadata_items
-        results = []
+        [].tap do |results|
+          read_tables.each do |table_name|
 
-        read_tables.each do |table_name|
-
-          request = {
-            table_name: table_name,
-            key_conditions: {
-              hash: {
-                attribute_value_list: [
-                  { s: owner_id }
-                ],
-                comparison_operator: "EQ"
-              },
-              range: {
-                attribute_value_list: [
-                  { s: "0" }
-                ],
-                comparison_operator: "BEGINS_WITH"
+            request = {
+              table_name: table_name,
+              key_conditions: {
+                hash: {
+                  attribute_value_list: [
+                    { s: owner_id }
+                  ],
+                  comparison_operator: "EQ"
+                },
+                range: {
+                  attribute_value_list: [
+                    { s: "0" }
+                  ],
+                  comparison_operator: "BEGINS_WITH"
+                }
               }
             }
-          }
 
-          dyna.dynamodb.query(request).items.each do |item|
-            results << MetadataItem.new(  table_name,
-                                          owner_id,
-                                          item["range"].s.gsub(/0\./, ""),
-                                          item["count"].s,
-                           MultiJson.load(item["payload"].s) )
+            dyna.dynamodb.query(request).items.each do |item|
+              results << MetadataItem.new(  table_name,
+                                            owner_id,
+                                            item["range"].s.gsub(/0\./, ""),
+                                            item["count"].s,
+                             MultiJson.load(item["payload"].s) )
+            end
+
           end
         end
-
-        results
       end
 
     end
